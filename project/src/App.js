866746b80ad0325c24ddd7f56bd869cb874bd0a2
DaniilTimonov <daniil.timonov1988@mail.ru>
2022-04-06 12:17:17 +0300

    видео на 38:03 минуте

--- project/src/App.js
+++ project/src/App.js
@@ -1,30 +1,27 @@
 import React, {useState} from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
-
+import './styles/App.css';
+import PostItem from './components/Postitem';
 
 function App() {
-const [likes, setLikes] = useState(5)
-const [value, setValue] = useState('Текст в инпуте')
+const [posts, setPosts] = useState([
+ {id:1, title:'Javascript', body:'Description'},
+ {id:2, title:'Javascript 2', body:'Description'},
+ {id:3, title:'Javascript 3', body:'Description'},
 
 
+])
+
+//test
   return (
     <div className="App">
-<div className = "post">
-  <div className = "post_content">
-    <strong>1. Javascript</strong>
-    <div>
-      Javascript - язык программирования
-    </div>
-    <div className = "post_btns">
-      <button>Удалить</button>
-    </div>
-  </div>
-</div>
-
+{posts.map(post =>
+<div> Post </div>
+)}
 
 </div>
-  );
+ );
 }
 export default App;
 
