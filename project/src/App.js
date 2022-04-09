7746e5fa24d4790facffe5e2ea8db5c0f6806b90
DaniilTimonov <daniil.timonov1988@mail.ru>
2022-04-09 20:35:24 +0300

    49:52 работа с уроком

--- project/src/App.js
+++ project/src/App.js
@@ -4,20 +4,41 @@ import Counter from './components/counter';
 import './styles/App.css';
 import PostItem from './components/Postitem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/button/input/MyInput';
 
 function App() {
 const [posts, setPosts] = useState([
  {id:1, title:'Javascript', body:'Description'},
  {id:2, title:'Javascript 2', body:'Description'},
  {id:3, title:'Javascript 3', body:'Description'},
-
-
 ])
+const [posts2, setPosts2] = useState([
+  {id:1, title:'Python', body:'Description'},
+  {id:2, title:'Python 2', body:'Description'},
+  {id:3, title:'Python 3', body:'Description'},
+ ])
+
+const [title, setTitle] = useState('deff' )
+
+const addNewPost = () => {
+     
+}
 
 
   return (
     <div className="App">
-<PostList posts={posts}/>
+  <form>
+    {/*Управляемый компонент*/}
+    <MyInput
+    value={title}
+    onChange = {e => setTitle(e.target.value)}
+    type="text" placeholder="Название поста"/>
+    <MyInput type="text" placeholder="Описание поста"/>
+    <MyButton onClick = {addNewPost}>создать пост</MyButton>
+  </form>
+<PostList posts={posts} title="Посты про JS"/>
+
 </div>
  );
 }
