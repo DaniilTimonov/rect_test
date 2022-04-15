88e9704a07fce1f978d6fa10ba0608cd5b9cb932
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-04-15 15:15:18 +0300

    1_10_24 видео урока js, логирование по выбору выпадающего меню

--- project/src/App.js
+++ project/src/App.js
@@ -8,6 +8,8 @@ import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/input/MyInput';
 import {useRef} from 'react';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
+
 
 
 function App() {
@@ -17,7 +19,7 @@ const [posts, setPosts] = useState([
  {id:3, title:'Javascript 3', body:'Description'},
 ])
 
-
+const [selectedSort, setSelectedSort] = useState('')
 const createPost = (newPost) => {
   setPosts (   [...posts, newPost])
 }
@@ -28,15 +30,31 @@ const removePost = (post) => {
  setPosts(posts.filter(p => p.id !== post.id))
 }
 
+const sortPosts = (sort) => {
+setSelectedSort(sort);
+console.log(sort)
+}
 
-
-  return (
+return (
     <div className="App">
 <PostForm create = {createPost} />
+<hr style = {{margin:'20px 0'}}/>
+<div>
+<MySelect
+  value = {selectedSort}
+  onChange = {sortPosts}
+  defaultValue="Сортировка"
+  options={[
+{value:'title', name: 'По названию'},
+{value:'body', name: 'По описанию'},
+  ]}
+/>
+</div>
 {posts.length !== 0
 ? <PostList remove={removePost}   posts={posts} title="Посты про JS"/>
 
-: <div> Посты не найдены! </div>
+: <h1 style={{textAlign: 'center'}}> 
+Посты не найдены! </h1>
 }
 </div>
  );
