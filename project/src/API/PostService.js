77c2a49a3ade933abfa00cf8f5f39a55825f6545
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-04-22 16:35:47 +0300

    2:00:03  Постраничный вывод. Пагинация

--- project/src/API/PostService.js
+++ project/src/API/PostService.js
@@ -1,15 +1,17 @@
 
 import axios from "axios";
 export default class PostService {
-  static async getAll() {
-    try {
+  static async getAll(limit = 10, page = 1 ) {
+    
 
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        return response.data
-    } catch (e) {
-        console.log(e);
-    }
-
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts',{
+          params: {
+            _limit : limit,
+            _page: page
+          }
+        } );
+        return response;
+  
 
   }
 
--- project/src/App.js
+++ project/src/App.js
@@ -16,6 +16,7 @@ import axios from 'axios';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount } from './utils/pages';
 
 
 
@@ -28,13 +29,18 @@ function App() {
 const [posts, setPosts] = useState(   [])
 const [filter, setfilter] = useState ({ sort: '', query: '' })
 const [modal, setModal] = useState(false);
+const [totalPages, setTotalPages] = useState(0);
+const [limit, setLimit] = useState(  10);
+const [page, setPage] = useState(  1);
 const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-/* const [isPostsLoading, setIsPostsLoading ] = useState(false); */
 const [fetchPosts, isPostsLoading, postError ] = useFetching ( async () => {
-  const posts = await PostService.getAll();
-  setPosts(posts);
+  const response = await PostService.getAll(limit, page);
+  setPosts(response.data);
+  const totalCount = response.headers['x-total-count']  
+  setTotalPages(getPageCount(totalCount, limit) );
 }  )
 
+console.log(totalPages)
 useEffect(()=> {
   fetchPosts()
 },[] )
@@ -67,8 +73,12 @@ return (
 <PostFilter
   filter = {filter}
   setFilter = {setfilter}
-
 />
+{postError &&
+<h1> Произошла ошибка ${postError} </h1>
+
+}
+
 {isPostsLoading
  ? <div style={{display:'flex', justifyContent:'center', marginTop : 50}}  > <Loader /> </div>
  : <PostList remove={removePost}   posts={sortedAndSearchedPosts} title="Посты про JS"/>
